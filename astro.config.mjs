dc0d2d9a7247f528fca5d55a82450bdc90716108
Ana Vukoja <vukojaa0@gmail.com>
2025-11-11 21:11:11 +0100

    početni commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+/*export default defineConfig({
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});*/
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
